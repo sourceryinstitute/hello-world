68fe65951b38815b1bae7ea377c34a3ee9dd70ee
Damian Rouson <rouson@lbl.gov>
2022-12-06 23:01:16 -0800

    feat: add co_broadcast in f2018 example

--- app/hello_2018.f90
+++ app/hello_2018.f90
@@ -2,14 +2,16 @@ program hello_2018
   use string_2018_m, only : string_t
   implicit none
   type(string_t) greeting
-  character(len=32) message[*]
+  character(len=32) message[*], salutation
   integer, parameter :: num_digits=12
   character(len=num_digits) image_number
   integer i
 
   associate(me => this_image())
-    write(image_number,*) this_image()
-    greeting = string_t("Hello from image" // image_number)
+    write(image_number,*) me
+    if (me==1) salutation = "Hello"
+    call co_broadcast(salutation, source_image=1)
+    greeting = string_t(trim(salutation) // " from image " // trim(adjustl(image_number)))
     message  = greeting%string()
     sync all
     if (me==1) then
